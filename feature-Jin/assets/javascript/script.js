fa0f1ea4cbf6a54a66b281e63625c2bc6bd4f5da
Kenny Tran <trank7295@gmail.com>
2019-03-16 08:17:28 -0400

    geo

--- feature-Jin/assets/javascript/script.js
+++ feature-Jin/assets/javascript/script.js
@@ -35,6 +35,7 @@ $(document).ready(function(){
           dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
       $("form")[0].reset();
+      getGeo();
   });
 
 
@@ -43,7 +44,7 @@ $(document).ready(function(){
     $("#add-row").append("<tr><td>" + childSnapshot.val().name +
     "</td><td>" + childSnapshot.val().location +
     "</td><td>" + childSnapshot.val().activity +
-    "</td><td>" + email + "</td></tr>");
+    "</td><td>" + childSnapshot.val().email + "</td></tr>");
 
 
 }, function(errorObject) {
@@ -61,73 +62,168 @@ $(document).ready(function(){
 
 
 //map js-ken
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 39.952583, lng: -75.165222},
-    zoom: 13,
-    mapTypeId: 'roadmap'
-  });
+var map = new google.maps.Map(document.getElementById('map'), {
+  center: { lat: -33.8688, lng: 151.2195 },
+  zoom: 13,
+  mapTypeId: 'roadmap',
+  styles: [
+      {
+          "featureType": "administrative.country",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#ffeb3b"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.government",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.medical",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.place_of_worship",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.school",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      }
+  ]
+});
 
+function initAutocomplete() {
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
   // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
+  map.addListener('bounds_changed', function () {
+      searchBox.setBounds(map.getBounds());
   });
-
   var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
+  searchBox.addListener('places_changed', function () {
+      var places = searchBox.getPlaces();
+      if (places.length == 0) {
+          return;
       }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+      // Clear out the old markers.
+      markers.forEach(function (marker) {
+          marker.setMap(null);
+      });
+      markers = [];
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function (place) {
+          if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+          }
+          var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+          };
+          // Create a marker for each place.
+          markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+          }));
 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
+          if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+          } else {
+              bounds.extend(place.geometry.location);
+          }
+      });
+      map.fitBounds(bounds);
   });
-  console.log('ok')
 }
+function getGeo() {
+  var infoWindow;
+  infoWindow = new google.maps.InfoWindow;
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+          var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+          };
+          infoWindow.open(map);
+          map.setCenter(pos);
+          var marker = new google.maps.Marker({
+              position: pos,
+              map: map,
+              title: 'Hello!'
+          });
+      }, function () {
+          handleLocationError(true, infoWindow, map.getCenter());
+      });
+  } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+  }
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+          'Error: The Geolocation service failed.' :
+          'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+  }
+};
 google.maps.event.addDomListener(window, "load", initAutocomplete);
 
 $(document).ready(function(){
